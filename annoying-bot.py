08ea6f7a8819a64dd3e5f37c253473e9e3d1cd7c
Epicalert <epicalert68@gmail.com>
2019-06-25 17:46:25 +0800

    added basic stuff

---
+++ annoying-bot.py
@@ -0,0 +1,55 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+bot = commands.Bot(command_prefix='oi mate ')
+
+@bot.event
+async def on_ready():
+    print("ready lol")
+    await bot.change_presence(activity=discord.Game('with your sanity'))
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def hai(self, ctx):
+        """hai"""
+        await ctx.send("hai :3")
+
+    @commands.command()
+    async def join(self, ctx):
+        """Joins a voice channel"""
+
+        channel = ctx.author.voice.channel
+
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+
+        await channel.connect()
+
+    @commands.command()
+    async def play(self, ctx, *, query):
+        """Plays a file from the local filesystem"""
+
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await ctx.send('Now playing: {}'.format(query))
+
+
+    @play.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
+bot.add_cog(Music(bot))
+bot.run("token")
