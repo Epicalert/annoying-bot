b45cd882ef38efbafafe5d083e0945360fcca0be
Epicalert <epicalert68@gmail.com>
2019-06-27 03:59:18 +0800

    added random voice channel annoyance

--- annoying-bot.py
+++ annoying-bot.py
@@ -4,11 +4,40 @@ from discord.ext.commands import Bot
 import asyncio
 import random
 import os
+import soundfile as sf
 
 import ttsfbfe.tts
 
+disconnectVoiceQueue = []
+
 bot = commands.Bot(command_prefix='oi mate ')
 
+def queueDisconnectVoice(guild):
+    disconnectVoiceQueue = disconnectVoiceQueue + [guild]
+
+def get_all_sendable_text_channels(self):
+    outlist = []
+    for server in self.guilds:
+        for channel in server.channels:
+            if channel.type == discord.ChannelType.text and channel.permissions_for(server.me).send_messages:
+                outlist = outlist + [channel]
+    return outlist
+
+def get_all_occupied_enterable_voice_channels(self):
+    outlist = []
+    for server in self.guilds:
+        for channel in server.channels:
+            if channel.type == discord.ChannelType.voice and channel.permissions_for(server.me).connect and channel.permissions_for(server.me).speak and len(channel.members) > 0:
+                outlist = outlist + [channel]
+    return outlist
+
+def getRandomTextChannel(self):
+    availableChannels = get_all_sendable_text_channels(self)
+    return availableChannels[random.randint(0, len(availableChannels) - 1)]
+
+def getRandomVoiceChannel(self):
+    availableChannels = get_all_occupied_enterable_voice_channels(self)
+    return availableChannels[random.randint(0, len(availableChannels) - 1)]
 
 async def annoyingAction_text(channel):
     annoyingPhrasesFile = open("annoyingPhrases.txt")
@@ -23,14 +52,25 @@ async def annoyingAction_image(channel):
 
     await channel.send("@everyone", file=discord.File("images/"+imageName))
 
+async def annoyingAction_voice(channel):
+    await channel.connect()
 
-def get_all_sendable_text_channels(self):
-    outlist = []
-    for server in self.guilds:
-        for channel in server.channels:
-            if channel.type == discord.ChannelType.text and channel.permissions_for(server.me).send_messages:
-                outlist = outlist + [channel]
-    return outlist
+    annoyingPhrasesFile = open("annoyingPhrases.txt")
+    annoyingPhrases = annoyingPhrasesFile.readlines()
+    annoyingPhrasesFile.close()
+    phraseToSay = annoyingPhrases[random.randint(0, len(annoyingPhrases) - 1)]
+
+    ttsfbfe.tts.runTTS(phraseToSay)
+
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("output.wav"))
+    channel.guild.voice_client.play(source)
+
+    outputsamples, outputrate = sf.read("output.wav")
+    outputDuration = int(len(outputsamples)/outputrate)
+
+    await asyncio.sleep(int(outputDuration) + 2) # REALLY hacky way do do it but meh
+
+    await channel.guild.voice_client.disconnect()
 
 @bot.event
 async def on_ready():
@@ -42,15 +82,21 @@ async def random_annoyance(self):
     print("random annoyances will start in 10 seconds.")
     await asyncio.sleep(10)
     while not self.is_closed():
-        availableTextChannels = get_all_sendable_text_channels(self)
-        channel = availableTextChannels[random.randint(0, len(availableTextChannels) - 1)]
+        channel = getRandomTextChannel(self)
 
         seversJoined = len(self.guilds)
 
-        if random.randint(0,1) == 0:
+        action = random.randint(0,2)
+        action = 2
+
+        if action == 0:
             await annoyingAction_text(channel)
-        else:
+        elif action == 1:
             await annoyingAction_image(channel)
+        else:
+            channel = getRandomVoiceChannel(self)
+            await annoyingAction_voice(channel)
+
 
         waittime = random.randint(int(15/seversJoined), int(1800/seversJoined))
         print("annoyance sent to " +channel.guild.name +"." +channel.name +"; next annoyance in " +str(waittime) +" seconds.")
